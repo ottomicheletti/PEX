14ce95802e4ba79527565b3e5d11db8ded382b49
Lucas Vollmann <lucasmvollmann@gmail.com>
2025-05-30 13:22:22 -0300

    Initial Firebase setup

---
+++ lib/firebase_options.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCIEY0AE1hai5LG-Z61SRKmXDmxbNUXHKM',
+    appId: '1:448494112107:android:26f6c7a33d1396cbd0902e',
+    messagingSenderId: '448494112107',
+    projectId: 'pex-agpop',
+    storageBucket: 'pex-agpop.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyC0zM4D4_5RHtTcCnOFWuDzKrNZLGRUzvA',
+    appId: '1:448494112107:ios:a764760cdfb96617d0902e',
+    messagingSenderId: '448494112107',
+    projectId: 'pex-agpop',
+    storageBucket: 'pex-agpop.firebasestorage.app',
+    iosBundleId: 'com.catolica.agpop',
+  );
+}
