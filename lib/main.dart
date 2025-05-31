4e9ad3b73e6e3a2dc3c77f7678e99969aa83a546
Otto Micheletti <michelettiotto@gmail.com>
2025-05-31 20:08:22 -0300

    utilizando .env no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:agpop/services/notification_service.dart';
 import 'package:agpop/firebase_options.dart';
 import 'package:agpop/routes.dart';
 import 'package:agpop/theme/app_theme.dart';
-import 'package:agpop/services/notification_service.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
